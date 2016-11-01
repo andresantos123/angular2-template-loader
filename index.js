6e110f860d788c982d3d985f76670d66747b9462
Leonardo Costa <leoccosta@outlook.com>
2016-11-01 08:38:54 -0200

    Dynamic templateUrl support

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 // using: regex, capture groups, and capture group variables.
-var templateUrlRegex = /templateUrl *:(.*)$/gm;
-var stylesRegex = /styleUrls *:(\s*\[[^\]]*?\])/g;
-var stringRegex = /(['"])((?:[^\\]\\\1|.)*?)\1/g;
+var loaderUtils = require('loader-utils');
+var fs = require('fs');
+var path = require('path');
 
-function replaceStringsWithRequires(string) {
+var templateUrlRegex = /templateUrl:(.*)$/gm;
+var stylesRegex = /styleUrls:(\s*\[[^\]]*?\])/g;
+var stringRegex = /(['"])((?:[^\\]\\\1|.)*?)\1/g;
+var templateMapRegex = /export const (.*?): string = (.*)$/gm;
+var templateMap = {};
+
+function loadTemplateFile(pathFile) {
+    var content = fs.readFileSync(path.normalize(pathFile), 'utf8');
+    return content;
+};
+
+function mapTemplateUrl(pathFile) {
+  var content = loadTemplateFile(pathFile);
+  var reg = new RegExp(templateMapRegex);
+  var result;
+  while(result = reg.exec(content)) {
+      templateMap[result[1]] = result[2];
+  }
+}
+
+function getTemplateUrlFromFile(pathFile, url) {
+  if (!Object.keys(templateMap).length){
+    mapTemplateUrl(pathFile, url)
+  }
+  var key = url.replace(/\s?template_url_\d\.(.*),$/g, '$1')
+  return templateMap[key];
+}
+
+function replaceStringsWithRequires(string, pathFile) {
+  if (pathFile && string.indexOf('template_url') > -1){
+    url = getTemplateUrlFromFile(pathFile, string);
+    return " require(" + url + "),";
+  }
+
   return string.replace(stringRegex, function (match, quote, url) {
     if (url.charAt(0) !== ".") {
       url = "./" + url;
@@ -13,19 +46,23 @@ function replaceStringsWithRequires(string) {
 }
 
 module.exports = function(source, sourcemap) {
-  // Not cacheable during unit tests;
-  this.cacheable && this.cacheable();
+    var self = this;
+    // Not cacheable during unit tests;
+    this.cacheable && this.cacheable();
 
-  var newSource = source.replace(templateUrlRegex, function (match, url) {
-                 // replace: templateUrl: './path/to/template.html'
-                 // with: template: require('./path/to/template.html')
-                 return "template:" + replaceStringsWithRequires(url);
-               })
-               .replace(stylesRegex, function (match, urls) {
-                 // replace: stylesUrl: ['./foo.css', "./baz.css", "./index.component.css"]
-                 // with: styles: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
-                 return "styles:" + replaceStringsWithRequires(urls);
-               });
+    // parse query params
+    var query = loaderUtils.parseQuery(self.query);
+    var newSource = source.replace(templateUrlRegex, function (match, url) {
+
+    // replace: templateUrl: './path/to/template.html'
+    // with: template: require('./path/to/template.html')
+    return "template:" + replaceStringsWithRequires(url, query.file);
+  })
+  .replace(stylesRegex, function (match, urls) {
+    // replace: stylesUrl: ['./foo.css', "./baz.css", "./index.component.css"]
+    // with: styles: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
+    return "styles:" + replaceStringsWithRequires(urls);
+  });
 
   // Support for tests
   if (this.callback) {
